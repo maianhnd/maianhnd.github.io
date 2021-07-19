55715f188970d8114cc4fc841e0bb30440a46078
maituyen <maithanhtuyen1@gmail.com>
2021-07-19 19:28:31 +0700

    maek it better

---
+++ js/javascript.js
@@ -0,0 +1,24 @@
+$(document).ready(function() {
+	$('.js-example-basic-multiple').select2();
+	$('[data-toggle="tooltip"]').tooltip();
+	$(function() {
+		$('input[name="daterange"]').daterangepicker({
+			opens: 'left'
+		}, function(start, end, label) {
+			console.log("A new date selection was made: " + start.format('YYYY-MM-DD') + ' to ' + end.format('YYYY-MM-DD'));
+		});
+
+	});
+	$('#data-table').DataTable( {
+        columnDefs: [ {
+            orderable: false,
+            className: 'select-checkbox',
+            targets:   0
+        } ],
+        select: {
+            style:    'os',
+            selector: 'td:first-child'
+        },
+        order: [[ 1, 'asc' ]]
+    } );
+});
